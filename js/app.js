334fb9f3f998881754c8c9c5bab8eae903b535ca
Thomask <s.thomas.kim@gmail.com>
2014-04-14 00:51:51 -0500

    Stuff seems to be working

--- js/app.js
+++ js/app.js
@@ -1,98 +1,85 @@
-window.App = Ember.Application.create();
-
-App.Router.map(function() {
-    this.resource('truth', { path: '/' });
-});
-
-App.TruthRoute = Ember.Route.extend ({
-    model: function() {
-        return App.Table.create()
-    }
-})
-
-App.Table = Ember.Object.extend ({
-    input: '',
-    exp_result: [],
-    parser: boolean_print,
-    ast_value: '',
-    ast: function() {
-        //console.log(this.get("parser.ast"))
-        try {
-            var output = new Array()
-            output = this.get("parser").parse(this.get("input")) //MyCustomJISONParser.parse(this.get(‘input’))
-            this.set("ast_value", output)
-            console.log(this.get("ast_value"))
-            return this.get("ast_value")
-        }
-        catch(err) {
-            console.log("Error getting input from textbox")
-            return this.get("ast_value")
-        }
+var parser = boolean_print;
+var input = "";
+var ast_arr = [];
+function get_ast(str) {
+    //console.log(this.get("parser.ast"))
+    try {
+        console.log(str);
+        var output = [];
+        input = str;
+        output = boolean_print.parse(input); //MyCustomJISONParser.parse(this.get(‘input’));
+        ast_arr = output;
         return output;
-    }.property('input', 'parser', 'ast_value'),
+    }
+    catch(err) {
+        console.log("Error getting input from textbox");
+        return ast_arr;
+    }
+}
 
-    input_vars: function() {
-        try {
-            output_arr = [];
-            str = this.get("input");
-            for(var i = 0; i < str.length; i++) {
-                if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z' && str.charAt(i) != 't' && str.charAt(i) != 'f') {
-                    output_arr.push(str.charAt(i));
-                }
+function input_vars(expr) {
+    try {
+        output_arr = [];
+        str = expr;
+        var i;
+        for(i = 0; i < str.length; i++) {
+            if(str.charAt(i) >= 'a' &&
+              str.charAt(i) <= 'z' &&
+              str.charAt(i) != 't' &&
+              str.charAt(i) != 'f') {
+                  output_arr.push(str.charAt(i));
             }
-            console.log(output_arr);
-            return output_arr;
         }
-        catch(err) {
-            console.log("error creating var list");
-            return [];
-        }
-    }.property('input'),
+        console.log(output_arr);
+        return output_arr;
+    }
+    catch(err) {
+        console.log("Error creating var list");
+        return [];
+    }
+}
 
-    echo_results: function() {
-        try {
-            console.log(this.get("exp_result"));
-            return exp_result;
-        }
-        catch(err) {
-            console.log("Error getting input from form");
-            return [];
-        }
-    }.property('exp_result')
-    /*
-    form: function BuildFormFields()
-    {
-        var $amount = 10;
-        var $container = document.getElementById('FormFields'),
-        $item, $field, $i;
+function get_results(){
+    try {
+        return [];
+    }
+    catch(err) {
+        console.log("Error getting input from form");
+        return [];
+    }
+}
 
-        $container.innerHTML = '';
-            for ($i = 0; $i < $amount; $i++) {
-                $item = document.createElement('div');
-                $item.style.margin = '3px';
+function build_form_fields(expr) {
+    var var_list = input_vars(expr);
+    var amount = var_list.length;
+    var container = document.getElementById('form_fields');
+    var item, field, i;
 
-                $field = document.createElement('span');
-                $field.innerHTML = 'Name of Design';
-                $field.style.marginRight = '10px';
-                $item.appendChild($field);
+    container.innerHTML = '';
+        for (i = 0; i < amount; i++) {
+            item = document.createElement('div');
+            item.style.margin = '3px';
 
-                $field = document.createElement('input');
-                $field.name = 'Design[' + $i + ']';
-                $field.type = 'text';
-                $item.appendChild($field);
+            field = document.createElement('span');
+            field.innerHTML = 'Name of Design';
+            field.style.marginRight = '10px';
+            item.appendChild(field);
 
-                $field = document.createElement('span');
-                $field.innerHTML = 'Quantity of Design';
-                $field.style.margin = '0px 10px';
-                $item.appendChild($field);
+            field = document.createElement('input');
+            field.name = 'Design[' + i + ']';
+            field.type = 'text';
+            item.appendChild(field);
 
-                $field = document.createElement('input');
-                $field.name = 'Quantity[' + $i + ']';
-                $field.type = 'text';
-                $item.appendChild($field);
+            field = document.createElement('span');
+            field.innerHTML = 'Quantity of Design';
+            field.style.margin = '0px 10px';
+            item.appendChild(field);
 
-                $container.appendChild($item);
-            }
-    }*/
+            field = document.createElement('input');
+            field.name = 'Quantity[' + i + ']';
+            field.type = 'text';
+            item.appendChild(field);
 
-})
+            container.appendChild(item);
+    }
+}
