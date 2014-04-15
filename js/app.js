d70e834024addfa6265cd7c14ffe9f79063f6754
Thomas Kim <s.thomas.kim@gmail.com>
2014-04-15 18:50:11 -0500

    Refactored code, fixed evaluation issue, needs testing

--- js/app.js
+++ js/app.js
@@ -172,77 +172,112 @@ function highlight_column() {
 }
 
 function build_form_fields() {
+    /* Get parameters from the URL */
     get_URL_params();
+    /* Get input variable list extracted from the URL parameters */
     var var_list = input_vars(g_input_str);
+    /* Get the subexpression list extracted from the URL parameters */
     var exp_list = get_ast(g_input_str);
+    /* get the form defined in html */
     var container = document.getElementById('form_fields');
-    var item, field, i, j,  num_rows, sub_item;
-    var num_cols;
-    var row_1, row_2;
-    var row_1_col, row_2_col;
+    /* item is the outermost table */
+    var item;
+    /* sub_item holds the inner table in each iteration of the loop */
+    var sub_item;
+    /* binding_index holds the index of the corresponding binding */
+    var row_1_binding_index;
+    var row_2_binding_index;
+    var binding_index;
+    /* row_1_num_cols holds the current number of columns in the subtable row 1 */
+    var row_1_num_cols;
+    /* row_2_num_cols holds the current number of columns in the subtable row 2 */
+    var row_2_num_cols;
+    /* row_1 holds the first row in the subtable */
+    var row_1;
+    /* row_2 holds the second row in the subtable */
+    var row_2;
+    /* row_1_col holds the current column in the first row of the subtable */
+    var row_1_col;
+    /* row_2_col holds the current column in the second row of the subtable */
+    var row_2_col;
+    var row_1_num_expr, row_2_num_expr;
     var item_row, item_row_cell;
     var input_box, expand_button;
     var bindings = get_initial_bindings(g_input_str);
-    i = 0;
 
     container.innerHTML = '';
 
     /* outer loop = enumerate bindings */
+    /* Create the outermost table */
     item = document.createElement('table');
     item.style.width='60%';
     num_tbls = 0;
-    j = 0;
+
+    row_1_binding_index = 0;
+    row_2_binding_index = 0;
+    binding_index = 0;
+
     while(bindings.length > 0) {
+        console.log(bindings);
+        row_1_num_cols = 0;
+        row_2_num_cols = 0;
+        row_1_num_expr = 0;
+        row_2_num_expr = 0;
+
         sub_item = document.createElement('table');
         sub_item.style.width='100%';
-        /* inner loop 1 = enumerate subexpressions */
-
         row_1 = sub_item.insertRow(0);
         row_2 = sub_item.insertRow(1);
+
         /* Put variable names in row 1 */
-        num_cols = 0;
-        while(num_cols < var_list.length) {
-            row_1_col = row_1.insertCell(num_cols);
-            row_1_col.innerHTML = var_list[num_cols];
-            ++num_cols;
+        while(row_1_num_cols < var_list.length) {
+            row_1_col = row_1.insertCell(row_1_num_cols);
+            row_1_col.innerHTML = var_list[row_1_num_cols];
+            ++row_1_num_cols;
         }
 
         /* put expressions in row 1 */
-        num_expr = 0;
-        while(num_expr < exp_list.length) {
-            row_1_col = row_1.insertCell(num_cols);
-            row_1_col.innerHTML = exp_list[num_expr];
+        while(row_1_num_expr < exp_list.length) {
+            row_1_col = row_1.insertCell(row_1_num_cols);
+            row_1_col.innerHTML = exp_list[row_1_num_expr];
             row_1_col.className = "subexps";
-            row_1_col.name = 'subexpr[' + i + ']';
-            ++num_cols;
-            ++num_expr;
+            row_1_col.name = 'subexpr[' + row_1_binding_index + ']';
+            ++row_1_binding_index;
+            ++row_1_num_cols;
+            ++row_1_num_expr;
         }
 
         /* Put variable bindings in row 2 */
-        num_cols = 0;
         bindings_set = false;
-        while(num_cols < var_list.length) {
-            row_2_col = row_2.insertCell(num_cols);
-            row_2_col.innerHTML = bindings[num_cols];
-            g_bindings_array[j] = bindings.slice();
-            ++i;
-            ++num_cols;
+        while(row_2_num_cols < var_list.length) {
+            row_2_col = row_2.insertCell(row_2_num_cols);
+            row_2_col.innerHTML = bindings[row_2_num_cols];
+            ++row_2_num_cols;
         }
 
         /* put expression input boxes in row 2 */
-        num_expr = 0;
-        while(num_expr < exp_list.length) {
+        while(row_2_num_expr < exp_list.length) {
+            /* declare input box */
             input_box = document.createElement("input");
             input_box.type = "text";
             input_box.className = "result_input";
-            input_box.name = 'subexpr_result[' + i + ']';
-            input_box.setAttribute("col_no",num_expr);
+            input_box.name = 'subexpr_result[' + row_2_binding_index + ']'; // wrong
+            input_box.setAttribute("col_no", row_2_num_expr);
             input_box.onkeyup = verify_input;
             input_box.onfocus = highlight_column;
-            row_2_col = row_2.insertCell(num_cols);
+            /* end declare input box */
+
+            row_2_col = row_2.insertCell(row_2_num_cols);
             row_2_col.appendChild(input_box);
-            ++num_expr;
-            ++num_cols;
+            ++row_2_num_expr;
+            ++row_2_num_cols;
+        }
+        /* create bindings */
+        var temp = 0;
+        while(temp < exp_list.length) {
+            g_bindings_array[binding_index] = bindings.slice();
+            ++binding_index;
+            ++temp;
         }
         /* insert the subtable as a row in the parent table */
         item_row = item.insertRow(num_tbls);
@@ -252,7 +287,7 @@ function build_form_fields() {
         ++num_tbls;
 
         bindings = get_next_bindings(bindings);
-        ++j;
     }
+    console.log(g_bindings_array);
     container.appendChild(item);
 }
