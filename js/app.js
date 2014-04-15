3154334fff1e5984cdf06df191730d76418a59f4
Bastion Fennell <bastionfennell@thefrontside.net>
2014-04-15 17:53:00 -0500

    Builds the table structure correctly now

--- js/app.js
+++ js/app.js
@@ -18,15 +18,37 @@ App.Table = Ember.Object.extend ({
         //console.log(this.get("parser.ast"))
         try {
             var output = new Array()
-            output = this.get("parser").parse(this.get("input")) //MyCustomJISONParser.parse(this.get(‘input’))
+            output = this.get("parser").parse(this.get("input"))
             this.set("ast_value", output)
-            console.log(this.get("ast_value"))
             return this.get("ast_value")
         }
         catch(err) {
-            console.log("error")
             return this.get("ast_value")
         }
         return output;
-    }.property('input', 'parser', 'ast_value')
+    }.property('input', 'parser', 'ast_value'),
+    variables: '',
+    number_list: function(){
+        var output = Ember.A([])
+        for(i = 0; i < Math.pow(2, (this.get("variables").length)); i++){
+            output.pushObject(i)
+        }
+        return output
+    }.property('variables')
+})
+
+App.TruthController = Ember.ObjectController.extend({
+    variable_array: function(){
+        variable_list = _.uniq(this.get("model").input.split(/[|&()\s]+/)).sort()
+        if(variable_list[0] === "")
+            variable_list.splice(0, 1)
+        this.set('model.variables', variable_list)
+        return variable_list
+    }.observes('model.input'),
+})
+
+App.TruthRowComponent = Ember.Component.extend({
+    variable_stuff: '',
+    ast_stuff: '',
+    row: null
 })
